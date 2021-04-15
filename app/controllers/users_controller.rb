82e1ca5821f3b3f20e91beda01cf722f8f555fca
EC2 Default User <ec2-user@ip-172-31-92-215.ec2.internal>
2021-04-15 14:47:42 +0000

    [Add]4-10共通テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+    @users = User.all
+  end
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to "/books"
+  end
+
+  def show
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
