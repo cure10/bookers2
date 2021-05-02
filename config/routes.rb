d90ebe321136ab4893505d5363f81f75dabfbc64
EC2 Default User <ec2-user@ip-172-31-92-215.ec2.internal>
2021-05-02 15:39:33 +0000

    [Start]応用課題３

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :homes, only: [:new, :create, :index]
   get 'home/about' => 'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
 end
