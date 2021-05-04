189cee6f0638900805c6626568852b092f82b3e9
EC2 Default User <ec2-user@ip-172-31-92-215.ec2.internal>
2021-05-04 06:22:22 +0000

    [Finish]応用課題４

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,24 @@
 class RelationshipsController < ApplicationController
-  def follow
-   current_user.follow(params[:id])
-   redirect_to root_path
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
 
-  def unfollow
-    current_user.unfollow(params[:id])
-    redirect_to root_path
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
   end
+
+  def followings
+    @user = current_user
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    @user = current_user
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:new, :create, :index, :show, :edit, :update, :followindex, :followerindex] do
-    resource :relationships, only: [:follow, :unfollow]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
 end
